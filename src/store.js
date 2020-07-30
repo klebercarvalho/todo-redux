f7caf35d35e4d08717d3a21cba857bd0a0f9db70
Kleber Rodrigo de Carvalho <kleber.carvalho@ibm.com>
2020-07-30 20:23:48 -0300

    add redux dev tools

--- src/store.js
+++ src/store.js
@@ -2,6 +2,12 @@ import { createStore } from 'redux';
 
 import reducers from './reducers';
 
-const store = createStore(reducers);
+/**
+ * Initialize Redux
+ */
+const store = createStore(
+  reducers /* preloadedState, */,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
